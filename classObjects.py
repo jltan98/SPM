a5cf4814d0f51e200e9ed18eae7f9c7bb21054c0
jltan98 <jialeng.tan.2019@scis.smu.edu.sg>
2021-10-30 03:33:40 +0800

    updated

--- classObjects.py
+++ classObjects.py
@@ -7,7 +7,7 @@ from datetime import date, datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root' + \
-                                        '@localhost:3306/learningmanagementsystem'
+                                        '@localhost:3306/LMS'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
 
