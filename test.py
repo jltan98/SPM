6b1ae9a7e0bddd88e3d48a092e5aba591c630a9a
croquetry <80682676+croquetry@users.noreply.github.com>
2021-11-07 16:15:41 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,37 @@
+import unittest
+from app import quiz, lesson
+
+class testwrongquestions(unittest.TestCase):
+    def setup(self):
+        self.quiz = quiz(1, "First Test", [True,True,False], "True", 3, 2)
+        
+    def tearDown(self):
+        self.quiz = None
+    
+    def checkwrongquestion(self):
+        wrongq = self.quiz.wrongquestiondict()
+        self.assertEqual(wrongq, [3])
+
+class testviewcontentlist(unittest.TestCase):
+    def setup(self):
+        self.lesson = lesson("1", "First,Second,Third", [True,True,False])
+    def tearDown(self):
+        self.lesson = None
+    def checkcontentlist(self):
+        self.assertEqual(self.lesson.json(), {
+            'lessonID': 1,
+            'title': "First,Second,Third",
+            'viewStatus': [True,True,False]
+        })
+
+class testprogress(unittest.TestCase):
+    def setup(self):
+        self.lesson = lesson("1", "First,Second,Third", [True,True,False])
+    def tearDown(self):
+        self.lesson = None
+    def checkprogress(self):
+        progress = self.lesson.progress()
+        self.assertEqual(progress, (2/3*100))
+
+if __name__ == "__main__":
+    unittest.main()
