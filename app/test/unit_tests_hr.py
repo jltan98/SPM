07f57d29d2785f2a390c984a382b39aec2f61b56
jltan98 <jialeng.tan.2019@scis.smu.edu.sg>
2021-11-08 12:55:11 +0800

    updated

--- app/test/unit_tests_hr.py
+++ app/test/unit_tests_hr.py
@@ -4,11 +4,7 @@ import flask_testing
 from datetime import datetime
 sys.path.append('./app')
 if True:  # noqa: E402
-    from src.classobj import Learner, Trainer, Administrator
-    from src.classobj import Classes, Course, Application, enrolmentPeriod
-    from src.enums import ClassesStatus
-    from src.classobj import db, app, Learner, Application
-    import src.enrolment
+    from src.enrolment import db, app, Learner, Application
 
 
 class TestApp(flask_testing.TestCase):
