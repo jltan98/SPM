517b0afb4e79ff703148db951a6c6c7d8f9b00f9
icedbluw <alvinngow21@gmail.com>
2021-11-08 18:36:19 +0800

    quizzes ui

---
+++ UI/quiz_creator_vue.js
@@ -0,0 +1,45 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            classes:[],
+            selected:null,
+            selected_sections:[],
+            section:null,
+            questions:null
+            
+        }
+    },
+    methods:{
+        get_sections(){
+            this.selected_sections = []
+            for(sections of this.classes){
+                if(sections["course"] == this.selected){
+                this.selected_sections.push(sections["section"])
+                }
+            }
+        }
+    },
+    mounted () {
+        console.log("start")
+        axios.get('http://localhost:5002/classes/T001', {
+    })
+        .then(response => {
+            data = response.data.data
+            for(sections of data){
+                section = sections.classID
+                course = sections.courseID
+                const obj = {course:course,section:section}
+                this.classes.push(obj)
+            }
+
+            console.log(this.classes[0].course)
+
+        })
+        .catch(error => {
+            console.log(error)
+        });
+      }
+
+})
+
+const vm = app.mount("#app");
---
+++ engineers/test_profile.py
@@ -0,0 +1,3 @@
+import unittest
+from profile import Courses,Enrol_Period
+
