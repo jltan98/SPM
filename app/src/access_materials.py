5ece8a8b95a5420567304292240be3d8b08b2180
jltan98 <jialeng.tan.2019@scis.smu.edu.sg>
2021-11-09 17:59:55 +0800

    update first

--- app/src/access_materials.py
+++ app/src/access_materials.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('dbURL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
-                                           'pool_recycle': 280}
+                                           'pool_recycle': 299}
 
 db = SQLAlchemy(app)
 
