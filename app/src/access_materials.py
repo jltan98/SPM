6a4674933ddc25c3b5c352345466640671702c64
jltan98 <jialeng.tan.2019@scis.smu.edu.sg>
2021-11-09 16:45:02 +0800

    change format

--- app/src/access_materials.py
+++ app/src/access_materials.py
@@ -27,19 +27,19 @@ class quiz(db.Model):
     results = db.Column(db.Integer)
 
     def __init__(self,
-                quizID="",
-                title="",
-                questionStatus="",
-                attempted="",
-                maxMarks="",
-                results=""):
+                 quizID="",
+                 title="",
+                 questionStatus="",
+                 attempted="",
+                 maxMarks="",
+                 results=""):
         self.quizID = quizID
         self.title = title
         self.questionStatus = questionStatus
         self.attempted = attempted
         self.maxMarks = maxMarks
         self.results = results
-    
+
     def json(self):
         return{
             'quizID': self.quizID,
@@ -49,40 +49,39 @@ class quiz(db.Model):
             'maxMarks': self.maxMarks,
             'results': self.results
         }
-            
+
     def wrongquestiondict(self):
         wrongq = []
         i = 1
-        #retrieve all the wrong qns tgt
+        # retrieve all the wrong qns tgt
         for y in self.questionStatus:
-            if y == False:
+            if y is False:
                 wrongq.append(i)
             i += 1
         return wrongq
-        
+
 
 class lesson(db.Model):
-    __tablename__ = 'lesson' 
-    #need to copy gab's list
+    __tablename__ = 'lesson'
     lessonID = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(20))
     viewStatus = db.Column(db.Boolean)
 
     def __init__(self,
-                lessonID="",
-                title="",
-                viewStatus=""):
+                 lessonID="",
+                 title="",
+                 viewStatus=""):
         self.lessonID = lessonID
         self.title = title
         self.viewStatus = viewStatus
-    
+
     def json(self):
         return{
             'lessonID': self.lessonID,
             'title': self.title,
             'viewStatus': self.viewStatus,
         }
-    #to give list of content
+    
     def to_dict(self):
         """
         'to_dict' converts the object into a dictionary,
@@ -93,14 +92,15 @@ class lesson(db.Model):
         for column in columns:
             result[column] = getattr(self, column)
         return result
-        
+
     def progress(self):
         a = 0
         for i in self.viewStatus:
-            if i == True:
+            if i is True:
                 a += 1
         progess = (a / (len(self.viewStatus))) * 100
         return progess
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003, debug=True)
