ff8d18a3db0d13a49a3830aea501df688b18717a
jltan98 <jialeng.tan.2019@scis.smu.edu.sg>
2021-11-10 18:51:23 +0800

    test

--- app/test/test_quizzes.py
+++ app/test/test_quizzes.py
@@ -44,19 +44,18 @@ class TestQuizzes(TestApp):
                                     data=json.dumps(request_body),
                                     content_type='application/json')
 
-        self.assertEqual(response.json,
-                        {"quizID": 2,
-                         "classID": "IS111",
-                         "sectionID": "G6",
-                         "active": 1,
-                         "answer": "Chicken",
-                         "questionNumber": 1,
-                         "question": "What came first?",
-                         "selections": {"selection":
-                                        ["chicken",
-                                         "egg",
-                                         "hen",
-                                         "rooster"]}})
+        self.assertEqual(response.json, {"quizID": 2,
+                                         "classID": "IS111",
+                                         "sectionID": "G6",
+                                         "active": 1,
+                                         "answer": "Chicken",
+                                         "questionNumber": 1,
+                                         "question": "What came first?",
+                                         "selections": {"selection":
+                                                        ["chicken",
+                                                         "egg",
+                                                         "hen",
+                                                         "rooster"]}})
 
 
 if __name__ == '__main__':
