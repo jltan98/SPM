1c8983a896fca082f906ad87bd6c81c9d7790089
jltan98 <jialeng.tan.2019@scis.smu.edu.sg>
2021-11-10 16:34:52 +0800

    test

--- app/test/test_quizzes.py
+++ app/test/test_quizzes.py
@@ -43,7 +43,7 @@ class TestQuizzes(TestApp):
         # db.session.add(qi)
         # db.session.commit()
 
-        request_body = [{
+        request_body = {
                 "quizID": 2,
                 "classID": "IS111",
                 "sectionID": "G6",
@@ -52,7 +52,7 @@ class TestQuizzes(TestApp):
                 "questionNumber": 1,
                 "question": "What came first?",
                 "selections": {"selection": ["chicken", "egg", "hen", "rooster"]}
-        }]
+        }
 
         response = self.client.post("/enter_quiz",
                                     data=json.dumps(request_body),
