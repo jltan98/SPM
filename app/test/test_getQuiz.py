8d8ffcf59315e571df3a621c4117774022eaaa53
jltan98 <jialeng.tan.2019@scis.smu.edu.sg>
2021-11-10 10:55:24 +0800

    test

--- app/test/test_getQuiz.py
+++ app/test/test_getQuiz.py
@@ -36,11 +36,11 @@ class TestLearner(TestApp):
 
         url = "/quiz"
         response = self.client.get(url)
-        quiz_dict = response.data
+        quiz_dict = response.data.data
         expectedValue = {'quizID': 2,
                          'classID': "IS111",
                          'sectionID': "G6",
-                         'active': 1,
+                         'active': True,
                         }
         self.assertEqual(expectedValue, quiz_dict)
 
